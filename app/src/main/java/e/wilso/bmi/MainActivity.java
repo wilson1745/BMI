2f0e52f94965f0efce7b8db7d7037cb14347d479
wilson1745 <wilson-power0130@hotmail.com>
2018-05-08 18:32:27 +0100

    Obseve Activity lifecycle

--- app/src/main/java/e/wilso/bmi/MainActivity.java
+++ app/src/main/java/e/wilso/bmi/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
+      Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
       setContentView(R.layout.activity_main);
 
       findViews();
@@ -73,6 +74,43 @@ public class MainActivity extends AppCompatActivity {
                  .show();*/
       }
       else Toast.makeText(this, "Toast：Something cannot be null", Toast.LENGTH_LONG).show();
+   }
+
+   @Override
+   protected void onStop() {
+      super.onStop();
+      Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+
+   }
+
+   @Override
+   protected void onDestroy() {
+      super.onDestroy();
+      Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+   }
+
+   @Override
+   protected void onPause() {
+      super.onPause();
+      Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+   }
+
+   @Override
+   protected void onResume() {
+      super.onResume();
+      Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+   }
+
+   @Override
+   protected void onStart() {
+      super.onStart();
+      Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+   }
+
+   @Override
+   protected void onRestart() {
+      super.onRestart();
+      Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
    }
 }
 
